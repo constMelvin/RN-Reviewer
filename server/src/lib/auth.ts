539e92c0715e4f6dca6de572146079d2ceda9d61
Melvin <domingo.jm029@gmail.com>
2026-04-05 23:00:02 +0800

    fix: fixed OAuth

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -29,7 +29,7 @@ export const auth = betterAuth({
 			prompt: "select_account",
 			clientId: envConfig.GOOGLE_CLIENT_ID,
 			clientSecret: envConfig.GOOGLE_CLIENT_SECRET,
-			// redirectURI: `${envConfig.FRONTEND_URL}/api/auth/callback/google`,
+			redirectURI: `${envConfig.FRONTEND_URL}/api/auth/callback/google`,
 		},
 	},
 });

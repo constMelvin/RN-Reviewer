886b7d3f660b31771cc8402747bd6912a09d6b0f
John Melvin Domingo <domingo.jm029@gmail.com>
2026-06-09 22:37:16 +0800

    back to old setup

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,18 +1,19 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { createDbClient } from "../db/create-db-client";
-import { oAuthProxy, openAPI, username } from "better-auth/plugins";
+import { openAPI, username } from "better-auth/plugins";
 import { envConfig } from "../env";
+
+
+const frontendUrls = envConfig.FRONTEND_URL.split(",").map(u => u.trim());
+const primaryFrontend = frontendUrls[0];
 
 export const auth = betterAuth({
     database: drizzleAdapter(createDbClient(), {
         provider: "pg",
     }),
     emailAndPassword: { enabled: true },
-    plugins: [openAPI(), username(), oAuthProxy({
-        productionURL: envConfig.BETTER_AUTH_URL,
-        secret: envConfig.OAUTH_PROXY_SECRET,
-    })],
+    plugins: [openAPI(), username()],
     trustedOrigins: [
         "http://localhost:3000",
         ...envConfig.FRONTEND_URL.split(",").map(u => u.trim()),
@@ -36,6 +37,7 @@ export const auth = betterAuth({
             prompt: "select_account",
             clientId: envConfig.GOOGLE_CLIENT_ID,
             clientSecret: envConfig.GOOGLE_CLIENT_SECRET,
+            redirectURI: `${primaryFrontend}/api/auth/callback/google`,
         },
     },
 });
